35c04e349f664957b2d0cc45f9e0ee20318341e9
Columbia91 <alpha.z91@mail.ru>
2019-03-25 21:09:58 +0600

    Добавьте файлы проекта.

---
+++ BankSystemDelegateLesson/BankAccount.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BankSystemDelegateLesson
+{
+    public delegate void ReporterDelegate(string reportText);
+    public class BankAccount
+    {
+        private ReporterDelegate reporterDelegate;
+        public string FullName { get; set; }
+        public int Sum { get; private set; }
+        public void RegisterDelegate(ReporterDelegate reporter)
+        {
+            //Delegate.Combine(reporterDelegate, reporter);
+            reporterDelegate += reporter;
+        }
+        public void UnregisterDelegate(ReporterDelegate reporter)
+        {
+            //Delegate.Remove(reporterDelegate, reporter);
+            reporterDelegate -= reporter;
+        }
+        public BankAccount()
+        {}
+
+        public BankAccount(string fullName, int sum)
+        {
+            FullName = fullName;
+            Sum = sum;
+        }
+
+        public void Add(int sum)
+        {
+            Sum += sum;
+            reporterDelegate?.Invoke($"На ваш поступило {sum} тг");
+        }
+        public void Withdraw(int sum)
+        {
+            if(sum <= Sum)
+            {
+                Sum -= sum;
+                reporterDelegate?.Invoke($"С вашего счета списано {sum} тг");
+                return;
+            }
+            reporterDelegate?.Invoke("Недостаточно средств для списания");
+        }
+    }
+}
---
+++ BankSystemDelegateLesson/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystemDelegateLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount
+            {
+                FullName = "Петр Иванович Жмых",
+            };
+
+            var consoleReporter = new ConsoleReport();
+            var reporterDelegate = new ReporterDelegate(consoleReporter.SendReport);
+            account.RegisterDelegate(reporterDelegate);
+            account.RegisterDelegate(new ReporterDelegate(Console.WriteLine));
+
+            account.Add(1000);
+            account.Withdraw(5000);
+            account.Withdraw(500);
+        }
+    }
+}
